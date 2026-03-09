fb949ff0f49025a0995845775fa28d61f9f8fc93
Fernando <fer@example.com>
2026-03-09 16:05:47 -0400

    Final changes payment succed pop up v3

--- public/js/pages/page3.js
+++ public/js/pages/page3.js
@@ -514,7 +514,7 @@ async function handlePayment() {
       const emailInput = getElementById('Page3_EmailInput');
       const buyerEmail = emailInput ? emailInput.value.trim() : '';
       if (buyerEmail) {
-        setTimeout(() => showEmailToast(lang), 10000);
+        setTimeout(() => showEmailToast(lang), 300);
       }
       
     } catch (pollError) {
